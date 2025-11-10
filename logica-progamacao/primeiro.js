0b4ed54d3570667bee26b24e8b0994c6478ea9ab
Aluno <euardosantanadasilva0@gmail.com>
2025-11-10 15:12:39 -0300

    Adicionando Arquivo

---
+++ logica-progamacao/primeiro.js
@@ -0,0 +1,12 @@
+// Pergutando o nome para o usuario
+const userName = prompt("Qual é seu nome?")
+
+// Perguntando qual é sua idade
+const userAge = prompt("Qual é sua Idade?");
+
+
+//Imprimindo na tela o resultado
+document.write("Seu nomé é: " + " <strong>" + userName + "</strong>" + "<br>" );
+
+// Imprimindo na tela a idade
+document.write("Sua idade é: " + " <strong>" + userAge + "</strong>" + " Anos" );
