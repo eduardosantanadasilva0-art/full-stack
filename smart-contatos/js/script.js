056a262a0aedad3bb505cb5a265888ea3cbf11e1
Aluno <euardosantanadasilva0@gmail.com>
2025-11-17 16:31:54 -0300

    Adicionando Arquivo

---
+++ smart-contatos/js/script.js
@@ -0,0 +1,16 @@
+const inputNome = document.getElementById("nome")
+const inputEmail = document.getElementById("email")
+
+    function btnCadastrar (event) {
+        event.preventDefault()
+        console.log(inputNome.value)
+
+    }
+
+   
+
+    function btnCadastrar (event) {
+        event.preventDefault()
+        console.log(inputEmail.value)
+
+    } 
